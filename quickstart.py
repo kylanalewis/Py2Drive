904961209672bc35367d3b1755eece9bf96ed852
Kylan Lewis <45674401+kylanlewis@users.noreply.github.com>
2021-03-30 18:10:55 -0500

    Add files via upload

---
+++ quickstart.py
@@ -0,0 +1,130 @@
+from __future__ import print_function
+import httplib2
+import os
+import os.path
+
+from apiclient import discovery
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
+from apiclient.http import MediaFileUpload, MediaIoBaseDownload
+try:
+    import argparse
+    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+except ImportError:
+    flags = None
+import auth
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+CLIENT_SECRET_FILE = 'credentials.json'
+APPLICATION_NAME = 'Drive API Python Quickstart'
+authInst = auth.auth(SCOPES,CLIENT_SECRET_FILE,APPLICATION_NAME)
+credentials = authInst.getCredentials()
+
+http = credentials.authorize(httplib2.Http())
+drive_service = discovery.build('drive', 'v3', http=http)
+
+def listFiles(size):
+    results = drive_service.files().list(
+        pageSize=size,fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print('{0} ({1})'.format(item['name'], item['id']))
+
+def uploadFile(filename,filepath,mimetype): # Change the date & time 
+    date = "2021-03-08T16:17:52.000Z"
+    file_metadata = {'name': filename, 'createdTime': date,
+    'modifiedTime': date, }
+    media = MediaFileUpload(filepath,
+                            mimetype=mimetype)
+    file = drive_service.files().create(body=file_metadata,
+                                        media_body=media,
+                                        fields='id').execute()
+    print('File ID: %s' % file.get('id'))
+
+def downloadFile(file_id,filepath):
+    request = drive_service.files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, request)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+        print("Download %d%%." % int(status.progress() * 100))
+    with io.open(filepath,'wb') as f:
+        fh.seek(0)
+        f.write(fh.read())
+
+def createFolder(name):
+    file_metadata = {
+    'name': name,
+    'mimeType': 'application/vnd.google-apps.folder'
+    }
+    file = drive_service.files().create(body=file_metadata,
+                                        fields='id').execute()
+    print ('Folder ID: %s' % file.get('id'))
+
+def searchFile(size,query):
+    results = drive_service.files().list(
+    pageSize=size,fields="nextPageToken, files(id, name, kind, mimeType)",q=query).execute()
+    items = results.get('files', [])
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(item)
+            print('{0} ({1})'.format(item['name'], item['id']))
+#uploadFile('unnamed.jpg','unnamed.jpg','image/jpeg')
+#downloadFile('1Knxs5kRAMnoH5fivGeNsdrj_SIgLiqzV','google.jpg')
+#createFolder('Google')
+searchFile(10,"name contains 'Getting'")
+
+def main():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('drive', 'v3', credentials=creds)
+
+    # Call the Drive v3 API
+    results = service.files().list(
+        pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    items = results.get('files', [])
+
+    if not items:
+        print('No files found.')
+    else:
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+
+#uploadFile('video.mkv','C:/Users/User/video.mkv','video/mkv')
+
+if __name__ == '__main__':
+    main()
